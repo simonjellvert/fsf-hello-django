b25d0b4ec84275270f79572c4521f868182af84b
simonjellvert <>
2023-11-01 09:20:03 +0000

    Add changes to setting.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -32,16 +32,14 @@ SECRET_KEY = os.environ.get(
     )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = development in os.environ
+DEBUG = development
 
-ALLOWED_HOSTS = ['localhost', os.environ.get('HEROKU_HOSTNAME')]
-
-# if development:
-#     ALLOWED_HOSTS = [
-#         '8000-simonjellvert-fsf-hello-ujh2clx8ka.us2.codeanyapp.com'
-#         ]
-# else:
-#     ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+if development:
+    ALLOWED_HOSTS = [
+        '8000-simonjellvert-fsf-hello-ujh2clx8ka.us2.codeanyapp.com'
+        ]
+else:
+    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 # Application definition
 
